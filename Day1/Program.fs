4bf8a52cc273f5260e85f1e082dbe8522c45f493
Nicolas Ding <niding@microsoft.com>
2021-12-02 00:43:13 +0100

    Add benchmarking tools

--- Day1/Program.fs
+++ Day1/Program.fs
@@ -1,4 +1,7 @@
 ﻿open System
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+
 type ENV = T | P
 
 let env = P
@@ -9,38 +12,6 @@ let inputFile = env |> function
 let lines = System.IO.File.ReadLines inputFile
                 |> Seq.map int
 
-//let result1a = 
-//    lines
-//    |> Seq.pairwise
-//    |> Seq.filter (fun (a, b) -> b > a)
-//    |> Seq.length
-
-//printfn "part1 : %d" result1a
-
-//let rec pb1a list = 
-//    match list with 
-//    | start::second::tail -> Convert.ToInt32(second > start) + pb1a (second::tail)
-//    | [_] -> 0
-
-//printfn "part1 : %d" (pb1a (List.ofSeq lines))
-
-//let result2a = 
-//    lines
-//    |> Seq.windowed 3
-//    |> Seq.pairwise
-//    |> Seq.filter (fun (a, b) -> (b |> Array.sum) > (a |> Array.sum))
-//    |> Seq.length
-
-//printfn "part 2 : %d" result2a
-
-//let result2b = 
-//    lines
-//    |> Seq.windowed 4
-//    |> Seq.filter (fun (array) -> Array.last array > array[0])
-//    |> Seq.length
-
-//printfn "part 2 : %d" result2b
-
 let day1 n = 
     lines
     |> Seq.windowed n
@@ -49,3 +20,17 @@ let day1 n =
 
 printfn "%d" (day1 2)
 printfn "%d" (day1 4)
+
+[<MemoryDiagnoser>]
+type Day1() = 
+
+    [<Benchmark>]
+    member self.Part1 () = day1 2
+
+    [<Benchmark>]
+    member self.Part2 () = day1 4
+
+[<EntryPoint>]
+let Main args =
+    BenchmarkRunner.Run typeof<Day1> |> ignore
+    0
